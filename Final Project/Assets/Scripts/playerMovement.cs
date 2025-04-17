83875b8c5b526303548456eb03c5ff6f00f47738
Evan Matherne <evanmatherne55@gmail.com>
2025-04-17 18:34:24 -0500

    Open with 2022.3.59f1

---
+++ Final Project/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,222 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Transform forwardTransform;
+    public Transform cameraTransform;
+    public Transform PovCameraTransform;
+    public bool isFirstPerson;
+
+    [Header("Moving")]
+    public bool isGrounded;
+    public float walkSpeed = 8f;
+    public float runSpeed = 18f;
+    float moveSpeed;
+    public float actualMoveSpeed;
+    Vector3 movement;
+    public Vector3 playerVelocity;
+    public float groundCheckDistance = 1.08f;
+    public float jumpHeight = 4f;
+    bool sprinting;
+    CharacterController controller;
+
+    [Header("Turning")]
+    public bool turningEnabled = true;
+    public float xSens = 5f;
+    public float ySens = 3f;
+    public float ySensPOV = 3f;
+    public Vector2 mouseMovement;
+    float xRotationCam;
+
+
+    [Header("Camera")]
+    public float distMult = 1f;
+    float distMultGoal = 1f;
+    public float heightAbove = 0f;
+        
+    float camHeight = 2f;
+    public float actualCamHeight;
+
+    float camDist = 8f;
+    public float actualCamDist;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        assignPlayerComponents();
+
+        isFirstPerson = PovCameraTransform;
+    }
+
+    void assignPlayerComponents(){
+        controller = GetComponent<CharacterController>();
+
+        if (transform.Find("Look Transform"))
+            forwardTransform = transform.Find("Look Transform");
+        
+        if (transform.Find("Player Camera"))
+            cameraTransform = transform.Find("Player Camera");
+        
+        if (transform.Find("POV"))
+            PovCameraTransform = transform.Find("POV");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad > 0.1f)
+        {
+            handleInput();
+            moving();
+            turning();
+        }
+
+        handleCamera();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+    void handleInput()
+    {
+        sprinting = (Input.GetKey(KeyCode.LeftShift));
+
+        if (Input.GetKeyDown(KeyCode.Space)) jumpOrDash();
+
+
+        distMultGoal -= Input.mouseScrollDelta.y / 10f;
+        distMultGoal = Mathf.Clamp(distMultGoal, 0.4f, 3f);
+    }
+
+
+    void moving()
+    {
+        moveSpeed = sprinting ? runSpeed : walkSpeed;
+        movement = forwardTransform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")));
+        if (movement.magnitude == 0) moveSpeed = 0f;
+
+
+        actualMoveSpeed += (moveSpeed - actualMoveSpeed) / 50f;
+
+        controller.Move(movement * actualMoveSpeed * Time.deltaTime);
+
+
+        if (!isGrounded) playerVelocity.y += Time.deltaTime * Physics.gravity.y;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+        // jumpHeight = -1 / ((actualMoveSpeed / 10f) + 0.3f) + 4.833f;
+
+        if (LayerMask.NameToLayer("Ground") == -1) // No Ground Layer Established
+            isGrounded = (controller.isGrounded || Physics.Raycast(transform.position, Vector3.down, groundCheckDistance));
+        else {                                     // Better, Ground Layer only if it exists
+            int groundLayer = LayerMask.NameToLayer("Ground");
+            isGrounded = (controller.isGrounded || Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, 1 << groundLayer)); //controller.isGrounded || 
+        }
+
+        if (isGrounded)
+        {
+            playerVelocity = Vector3.down * Mathf.Max(3f, -playerVelocity.y);
+            //print("GROUNDED");
+        }
+
+    }
+
+    void turning()
+    {
+        turningEnabled = !Cursor.visible;
+        mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        ///////////////////////////////////////////////////////////
+
+        float xTurn = mouseMovement[0] * xSens * Time.deltaTime;
+        float yTurn = mouseMovement[1] * ySens * Time.deltaTime;
+        float yTurnPOV = mouseMovement[1] * ySensPOV * Time.deltaTime;
+
+        if (turningEnabled)
+        {
+            // Left + Right
+            transform.Rotate(Vector3.up, xTurn);
+
+            // Up + Down
+            heightAbove += yTurn;
+            heightAbove = Mathf.Clamp(heightAbove, -1.5f, 3.2f);
+
+            xRotationCam -= yTurnPOV;
+            xRotationCam = Mathf.Clamp(xRotationCam, -90f, 75f);
+        }
+
+        ///////////////////////////////////////////////////////////
+
+    }
+
+    void handleCamera()
+    {
+        // DECIDE WHICH CAM IS ACTIVE
+        // cameraTransform.gameObject.SetActive(distMult > 0.45f); if (distMult < 0.45f) heightAbove = 2f;
+        // PovCameraTransform.gameObject.SetActive(!cameraTransform.gameObject.activeInHierarchy); if (distMult > 0.45f) xRotationCam = 0f;
+        //
+
+        if (isFirstPerson){
+            
+            // POV Camera
+            PovCameraTransform.localEulerAngles = new Vector3(xRotationCam, 0f, 0f);
+            return;
+        }
+
+        // Third Person Stuff
+        distMult += (distMultGoal - distMult) / 10f;
+
+        // DO NOT BE INSIDE A WALL
+        if (cameraTransform){
+            float distToCam = Vector3.Distance(transform.position, cameraTransform.position);
+            Vector3 toCamera = Vector3.Normalize(cameraTransform.position - transform.position); RaycastHit hit;
+            if (isGrounded && Physics.Raycast(transform.position, toCamera, out hit, distToCam * 1.25f)) // Check a bit farther
+            {
+                float DistToObject = Vector3.Distance(hit.point, transform.position) * 1.5f; // Pull camera in a bit from the point
+                camDist += (DistToObject - camDist) / 3f;
+            }
+        }
+        //
+
+        /// LEAVE ///
+        camHeight = -1.46f * heightAbove + 4.45f;
+        camDist = -Mathf.Pow((heightAbove + 1.5f) * 0.3f, 2) + 10f;
+        /////////////
+
+        actualCamHeight += (camHeight - actualCamHeight) / 10f;
+        actualCamDist += (camDist - actualCamDist) / 10f;
+
+        if (cameraTransform) {
+            cameraTransform.localPosition = new Vector3(0f, actualCamHeight, -actualCamDist * distMult);
+            // GetComponent<MeshRenderer>().enabled = distMult > 0.4f;
+            
+            cameraTransform.LookAt(transform.position + Vector3.up * heightAbove);
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+    void jumpOrDash()
+    {
+        if (isGrounded)
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+
+
+
+    }
+
+
+
+    
+}
