7688915cf25bd8f10b799003ec56256268120444
Masonkta <88817850+Masonkta@users.noreply.github.com>
2025-04-18 02:24:56 -0500

    Imported TextMeshPro Asset, Started work on the Facility Chamber Scene, Created headlock logic so player can only view so far while in chair, Created tv ui setup.

---
+++ Final Project/Assets/Scripts/HeadLock.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HeadLook : MonoBehaviour
+{
+    public float sensitivity = 2f;
+    public Vector2 clampY = new Vector2(55f, 120f);    
+    public Vector2 clampX = new Vector2(-17f, 43f);    
+    private float rotationY = 0f; 
+    private float rotationX = 0f; 
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rotationY = transform.eulerAngles.y;
+        rotationX = transform.eulerAngles.x;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY += mouseX;
+        rotationX -= mouseY;
+        rotationY = Mathf.Clamp(rotationY, clampY.x, clampY.y);
+        rotationX = Mathf.Clamp(rotationX, clampX.x, clampX.y);
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0f);
+    }
+}
---
+++ Final Project/Assets/Scripts/UIClicker.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIClicker : MonoBehaviour
+{
+    public Camera playerCamera;
+    private Button currentlyHoveredButton;
+
+    void Update()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            position = new Vector2(Screen.width / 2, Screen.height / 2)
+        };
+
+        var raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, raycastResults);
+        Button hoveredButton = null;
+
+        foreach (RaycastResult result in raycastResults)
+        {
+            if (result.gameObject.CompareTag("UIButton"))
+            {
+                hoveredButton = result.gameObject.GetComponent<Button>();
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    hoveredButton.onClick.Invoke();
+                }
+                break; 
+            }
+        }
+
+        if (hoveredButton != currentlyHoveredButton)
+        {
+            if (currentlyHoveredButton != null)
+            {
+                SetButtonAlpha(currentlyHoveredButton, 0f); 
+            }
+            if (hoveredButton != null)
+            {
+                SetButtonAlpha(hoveredButton, 0.2f);
+            }
+            currentlyHoveredButton = hoveredButton;
+        }
+    }
+
+    void SetButtonAlpha(Button button, float alpha)
+    {
+        Image image = button.GetComponent<Image>();
+        if (image != null)
+        {
+            Color c = image.color;
+            c.a = alpha;
+            image.color = c;
+        }
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("START");
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Options");
+    }
+}
