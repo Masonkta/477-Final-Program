22380d17b84da9ee98c80eb84b72c8f7932bd943
Evan Matherne <58615631+CarlRox55@users.noreply.github.com>
2025-05-06 15:14:43 -0500

    cooking

--- Final Project/Assets/enemyScript.cs
+++ Final Project/Assets/enemyScript.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum Direction
+{
+    Straight,
+    Left,
+    Right,
+    Neighborhood
+}
 
 public class enemyScript : MonoBehaviour
 {   
     CharacterController controller;
     [Header("Points")]
-    public Transform point1;
+    public Direction direction;
 
     [Header("Other")]
     public Transform currentDestination;
-    public float moveSpeed;
+    public float moveSpeed = 3f;
+    float timeOfLastDestinationSet;
+    int pointsVisited = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        point1 = transform.parent.Find("Point 1");
-        currentDestination = point1;
+        currentDestination = transform.parent.Find("First Fork");
+        timeOfLastDestinationSet = Time.time;
     }
 
     // Update is called once per frame
@@ -29,6 +38,47 @@ public class enemyScript : MonoBehaviour
     void moveToCurrentPoint(){
         Vector3 moveDirection = currentDestination.position - transform.position; moveDirection.y = 0f;
 
+        // Only rotate if we're actually moving
+        if (moveDirection.sqrMagnitude > 0.001f) {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        }
+
         controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+                                                                    //  Dist from point
+        if (Vector3.Distance(transform.position, currentDestination.position) < 2f && Time.time - timeOfLastDestinationSet > 2f){ // Second timeout before destination updates can be made
+            selectNewDestination();
+            timeOfLastDestinationSet = Time.time;
+        }
+    }
+
+    void selectNewDestination(){
+        if (direction == Direction.Left){
+            if (pointsVisited == 0) currentDestination = transform.parent.Find("Left Point 1");
+            if (pointsVisited == 1) currentDestination = transform.parent.Find("Left Point 2");
+            if (pointsVisited == 2) currentDestination = transform.parent.Find("Back Of City");
+            pointsVisited += 1;
+        }
+
+        if (direction == Direction.Right){
+            if (pointsVisited == 0) currentDestination = transform.parent.Find("Right Point 1");
+            if (pointsVisited == 1) currentDestination = transform.parent.Find("Right Point 2");
+            pointsVisited += 1;
+        }
+        
+        if (direction == Direction.Straight){
+            if (pointsVisited == 0) currentDestination = transform.parent.Find("Straight Point 1");
+            if (pointsVisited == 1) currentDestination = transform.parent.Find("Straight Point 2");
+            
+            pointsVisited += 1;
+        }
+        
+        if (direction == Direction.Neighborhood){
+            if (pointsVisited == 0) currentDestination = transform.parent.Find("Straight Point 1");
+            if (pointsVisited == 1) currentDestination = transform.parent.Find("To Neighborhood");
+            if (pointsVisited == 2) currentDestination = transform.parent.Find("Inside Neighborhood");
+            
+            pointsVisited += 1;
+        }
     }
 }
