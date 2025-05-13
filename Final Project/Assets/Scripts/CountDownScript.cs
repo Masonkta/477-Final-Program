3553e06a16f40727416cd049eb796e6a382de30c
Evan Matherne <58615631+CarlRox55@users.noreply.github.com>
2025-05-13 14:08:31 -0500

    Clock Arms Move

--- Final Project/Assets/Scripts/CountDownScript.cs
+++ Final Project/Assets/Scripts/CountDownScript.cs
@@ -8,22 +8,28 @@ using UnityEngine.SceneManagement;
 public class CountDownScript : MonoBehaviour
 {
     public float timeLimit = 600; //inSeconds
-    float timeRemaining;
-    public DateTime timerValue;
+    public float timeElapsed = 0f;
+    // float timeRemaining;
+    // public DateTime timerValue;
     // Start is called before the first frame update
-    void Start()
-    {
-        timerValue = DateTime.Today.AddSeconds(timeLimit);
-        StartCoroutine(StartTimer());
+    void Update()
+    {   
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > timeLimit){
+            print("SCENE LOAD");
+            SceneManager.LoadScene("StartScene");
+        }
+        // timerValue = DateTime.Today.AddSeconds(timeLimit);
+        // StartCoroutine(StartTimer());
     }
 
-    IEnumerator StartTimer(){
-        timeRemaining = timeLimit;
-        while (timeRemaining > 0){
-            yield return new WaitForSeconds(1f);
-            timeRemaining -= 1f;
-            timerValue = DateTime.Today.AddSeconds(timeRemaining);
-        }
-        SceneManager.LoadScene("StartScene");
-    }
+    // IEnumerator StartTimer(){
+    //     timeRemaining = timeLimit;
+    //     while (timeRemaining > 0){
+    //         yield return new WaitForSeconds(1f);
+    //         timeRemaining -= 1f;
+    //         timerValue = DateTime.Today.AddSeconds(timeRemaining);
+    //     }
+    //     SceneManager.LoadScene("StartScene");
+    // }
 }
---
+++ Final Project/Assets/clockScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clockScript : MonoBehaviour
+{   
+    public float timeElapsed = 0f;
+    public float seconds;
+    public float minutes;
+    public float hours;
+    public Transform s1;
+    public Transform m1;
+    public Transform h1;
+    public Transform s2;
+    public Transform m2;
+    public Transform h2;
+    public Transform s3;
+    public Transform m3;
+    public Transform h3;
+    public Transform s4;
+    public Transform m4;
+    public Transform h4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        seconds = Mathf.RoundToInt(timeElapsed % 60f);
+        minutes = (timeElapsed / 60f) % 60f;
+        hours = (minutes / 60f) % 60f;
+
+        s1.localEulerAngles = new Vector3(0f, 0f, Map(seconds, 0, 60, 0f, -360f));
+        m1.localEulerAngles = new Vector3(0f, 0f, Map(minutes, 0, 60, 90f, -270f));
+        h1.localEulerAngles = new Vector3(0f, 0f, Map(hours, 0, 12, -180f, -540f));
+
+        s2.localEulerAngles = new Vector3(0f, 0f, Map(seconds, 0, 60, 0f, 360f));
+        m2.localEulerAngles = new Vector3(0f, 0f, Map(minutes, 0, 60, 270f, 360f+270f));
+        h2.localEulerAngles = new Vector3(0f, 0f, Map(hours, 0, 12, -180f, 180f));
+
+        s3.localEulerAngles = new Vector3(Map(seconds, 0, 60, 0f, -360f), 0f, 0f);
+        m3.localEulerAngles = new Vector3(Map(minutes, 0, 60, 90f, -360f+90f), 0f, 0f);
+        h3.localEulerAngles = new Vector3(Map(hours, 0, 12, 180f, -180f), 0f, 0f);
+        
+        s4.localEulerAngles = new Vector3( Map(seconds, 0, 60, 0f, 360f), 0f, 0f);
+        m4.localEulerAngles = new Vector3( Map(minutes, 0, 60, -90f, 270f), 0f, 0f);
+        h4.localEulerAngles = new Vector3( Map(hours, 0, 12, -180f, 180f), 0f, 0f);
+    }
+
+    
+    float Map(float value, float start1, float end1, float start2, float end2)
+    {
+        return (value - start1) / (end1 - start1) * (end2 - start2) + start2;
+    }
+}
